8c48cbdfad5b9a8957207298613fa5334e2b4234
peter <peterkamalu96@gmail.com>
2026-05-29 18:44:04 +0100

    updated all screen sizes

---
+++ script.js
@@ -0,0 +1,17 @@
+function greet(){}
+
+
+
+const text = () => "welcome peter!";
+
+
+// understanding javascript objects and methods
+
+const human = {
+    firstname: "Peter";
+    lastname:  "kamalu";
+    height : 15;
+
+    
+    
+}
